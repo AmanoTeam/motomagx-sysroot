5353beff3c0dccc5c7f7ec68bfbd96abcf1d5606
Kartatz <105828205+Kartatz@users.noreply.github.com>
2025-12-25 08:57:45 -0300

    Initial commit

---
+++ main.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+
+set -eu
+
+declare -r motomagx_tarball='/tmp/motomagx.tar.gz'
+declare -r motomagx_directory='/tmp/motomagx-SDK-toolchain-master'
+
+declare -r sysroot_directory='/tmp/arm-unknown-linux-gnueabi'
+declare -r tarball_filename="${sysroot_directory}.tar.xz"
+
+if ! [ -f "${motomagx_tarball}" ]; then
+	curl \
+		--url 'https://github.com/fillsa/motomagx-SDK-toolchain/archive/refs/heads/master.tar.gz' \
+		--retry '30' \
+		--retry-delay '0' \
+		--retry-all-errors \
+		--retry-max-time '0' \
+		--location \
+		--silent \
+		--output "${motomagx_tarball}"
+	
+	tar \
+		--directory="$(dirname "${motomagx_directory}")" \
+		--extract \
+		--file="${motomagx_tarball}"
+fi
+
+mkdir \
+	--parent \
+	"${sysroot_directory}/"{include,lib}
+
+mv \
+	"${motomagx_directory}/arm-linux-gnueabi/include" \
+	"${sysroot_directory}"
+
+mv \
+	"${motomagx_directory}/arm-linux-gnueabi/lib" \
+	"${sysroot_directory}"
+
+rm \
+	--force \
+	--recursive \
+	"${sysroot_directory}/lib/lib"{supc++,c++,gcc}* \
+	"${sysroot_directory}/include/"{c++,faad,x264} \
+	"${sysroot_directory}/lib/ldscripts"
+
+echo "- Creating tarball at ${tarball_filename}"
+
+tar --directory="$(dirname "${sysroot_directory}")" --create --file=- "$(basename "${sysroot_directory}")" | xz --compress -9 > "${tarball_filename}"
+sha256sum "${tarball_filename}" | sed "s|$(dirname "${sysroot_directory}")/||" > "${tarball_filename}.sha256"
+
+rm \
+	--force \
+	--recursive \
+	"${sysroot_directory}" \
+	"${motomagx_directory}" \
+	"${motomagx_tarball}"
